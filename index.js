6727071c27e9c613fe9f86f069c7c3f5c4b2dd40
DTGuy <tguy5837@gmail.com>
2022-10-14 23:51:21 -0400

    added view employees, role, and department

--- index.js
+++ index.js
@@ -25,8 +25,28 @@ const mainMenu = async () => {
                     value: 'VIEW_EMPLOYEES'
                 },
                 {
+                    name: 'View All Roles',
+                    value: 'VIEW_ROLES'
+                },
+                {
                     name: 'View All Departments',
-                    value: 'VIEW_DEPARTMENTS'
+                    value: 'VIEW_DEPARTMENT'
+                },
+                {
+                    name: 'Add an Employee',
+                    value: 'ADD_EMPLOYEE'
+                },
+                {
+                    name: 'Add a Role',
+                    value: 'ADD_ROLE'
+                },
+                {
+                    name: 'Add a Department',
+                    value: 'ADD_DEPARTMENT'
+                },
+                {
+                    name: 'Update Employee Role',
+                    value: 'UPDATE_EMPLOYEE_ROLE'
                 },
                 {
                     name: 'Exit',
@@ -40,23 +60,56 @@ const mainMenu = async () => {
         case 'VIEW_EMPLOYEES':
             viewEmployees();
             break;
+        case 'VIEW_ROLES':
+            viewRoles();
+            break;
         case 'VIEW_DEPARTMENT':
             viewDepartments();
             break;
-        case 'EXIT':
-            // do something
+        case 'ADD_EMPLOYEE':
+            addEmployee();
             break;
+        case 'ADD_ROLE':
+            addRole();
+            break;
+        case 'ADD_DEPARTMENT':
+            addDepartment();
+            break;
+        case 'UPDATE_EMPLOYEE_ROLE':
+            updateEmployeeRole();
+            break;
+        case 'EXIT':
+            process.exit();
         default:
             process.exit();
     }
 };
 
 const viewEmployees = async () => {
-    const [employeeData] = await db.query('SELECT * FROM employee;');
+    const [employeeData] = await db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department_name, role.salary, employee.manager_id
+                                            FROM employee
+                                            INNER JOIN role ON employee.role_id = role.id
+                                            INNER JOIN department ON role.id = department.id;`);
     console.table(employeeData);
+    mainMenu();
 };
-const viewDepartments = () => {
 
+const viewDepartments = async () => {
+    const [departmentData] = await db.query('SELECT * FROM department');
+    console.table(departmentData);
+    mainMenu();
 };
+
+const viewRoles = async () => {
+    const [roleData] = await db.query(`SELECT role.id, role.title, role.salary, department.name AS department_name 
+                                        FROM role 
+                                        INNER JOIN department ON role.department_id = department.id`);
+    console.table(roleData);
+    mainMenu();
+};
+
+const addEmployee = async () => {
+    //
+}
 
 mainMenu();
